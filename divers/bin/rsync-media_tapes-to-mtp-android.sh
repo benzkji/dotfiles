fca12897d518c52d3664fdc159d8eb93b8e75908
benzkji <bnzk@bnzk.ch>
2018-02-19 13:52:14 +0100

    rsync helpers for media / iTunes

---
+++ divers/bin/rsync-media_tapes-to-mtp-android.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+rsync --recursive --inplace --omit-dir-times --size-only --no-perms --verbose --progress --delete --ignore-existing /home/benzkji/Sync/media_tapes /run/user/$UID/gvfs/mtp*/SD*/my_media/
