5db2c3766bbd00473a09c49cb283a177a08363da
benzkji <bnzk@bnzk.ch>
2018-02-25 10:30:09 +0100

    sync!

--- divers/bin/rsync-media_alle-to-mtp-android.sh
+++ divers/bin/rsync-media_alle-to-mtp-android.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 
-rsync --recursive --inplace --omit-dir-times --size-only --no-perms --verbose --progress --delete --ignore-existing /home/benzkji/Sync/media_alle /run/user/$UID/gvfs/mtp*/SD*/my_media/
+rsync  \
+  --recursive \
+  --inplace \
+  --omit-dir-times \
+  --size-only \
+  --no-perms \
+  --verbose \
+  --progress \
+  --delete \
+  --ignore-existing \
+  /home/benzkji/Sync/media-alle \
+  /run/user/$UID/gvfs/mtp*/SD*/my-media/
--- divers/bin/rsync-media_tapes-to-mtp-android.sh
+++ divers/bin/rsync-media_tapes-to-mtp-android.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
 
-rsync --recursive --inplace --omit-dir-times --size-only --no-perms --verbose --progress --delete --ignore-existing /home/benzkji/Sync/media_tapes /run/user/$UID/gvfs/mtp*/SD*/my_media/
+rsync \
+  --recursive \
+  --inplace  \
+  --omit-dir-times \
+  --size-only  \
+  --no-perms \
+  --verbose \
+  --progress \
+  --delete \
+  --ignore-existing \
+  /home/benzkji/Sync/media-tapes\
+  /run/user/$UID/gvfs/mtp*/SD*/my-media/
